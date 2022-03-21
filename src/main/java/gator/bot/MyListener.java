36391c375aad04006d6b9d294b10b7d83576ea26
benny <bennycortese@gmail.com>
2022-03-21 19:25:35 -0400

    Added basic image downloading from attachments

--- src/main/java/gator/bot/MyListener.java
+++ src/main/java/gator/bot/MyListener.java
@@ -5,6 +5,10 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public class MyListener extends ListenerAdapter
 {
     @Override
@@ -14,12 +18,22 @@ public class MyListener extends ListenerAdapter
         // We don't want to respond to other bot accounts, including ourself
         Message message = event.getMessage();
         String content = message.getContentRaw();
+        List<Message.Attachment> attachments = event.getMessage().getAttachments();
         // getContentRaw() is an atomic getter
         // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
         if (content.equals("!ping"))
         {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+        }
+        if(content.equals("!image")) {
+            if(attachments.isEmpty())
+            {
+                return;
+            }
+            MessageChannel channel = event.getChannel();
+            CompletableFuture<File> future = attachments.get(0).downloadToFile(new File("./imageStore/" + "image.jpg"));
+            channel.sendMessage("Blue!").queue();
         }
     }
 }
