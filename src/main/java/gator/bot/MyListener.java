6e32a44eb7c6211ab03bf0634ee68f6b34ae916f
benny <bennycortese@gmail.com>
2022-04-15 16:20:23 -0400

    Added/fixed MyListener changes for merging

--- src/main/java/gator/bot/MyListener.java
+++ src/main/java/gator/bot/MyListener.java
@@ -1,14 +1,13 @@
 package gator.bot;
 
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.io.*;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
@@ -97,65 +96,39 @@ public class MyListener extends ListenerAdapter
                 e.printStackTrace();
             }
         }
-        java.util.List<Role> authorRole = event.getMember().getRoles();
-        Role roleCreator = event.getGuild().getRoleById("958889828118851645");
-        Role teacher = event.getGuild().getRoleById("958912483647684668");
-        Role student = event.getGuild().getRoleById("958890976640909352");
-        Role ta = event.getGuild().getRoleById("958912619098562620");
-        Role author = authorRole.get(0);
-        if(content.startsWith("!changeToRoleCreator")){
-            if(author == roleCreator){
-                java.util.List<Member> move = message.getMentionedMembers();
-                for (Member member : move) {
-                    java.util.List<Role> curRole = member.getRoles();
-                    event.getGuild().addRoleToMember(member, roleCreator).queue();
-                    event.getGuild().removeRoleFromMember(member, curRole.get(0)).queue();
-                    MessageChannel channel = event.getChannel();
-                    channel.sendMessage("Success!").queue();
-                }
-            }
-            else {
-                MessageChannel channel = event.getChannel();
-                channel.sendMessage("You do not have permission to do this!").queue();
-            }
+        if(Pattern.matches("!createTextChannel .*", content))
+        {
+            Member member = event.getMember();
+            createTextChannel(member, content.substring(19));
         }
-
-        if(author == teacher || author == roleCreator) {
-            if (content.startsWith("!changeToTeacher")) {
-                java.util.List<Member> move = message.getMentionedMembers();
-                for (Member member : move) {
-                    java.util.List<Role> curRole = member.getRoles();
-                    event.getGuild().addRoleToMember(member, teacher).queue();
-                    event.getGuild().removeRoleFromMember(member, curRole.get(0)).queue();
-                    MessageChannel channel = event.getChannel();
-                    channel.sendMessage("Success!").queue();
-                }
-
-            }
-            if (content.startsWith("!changeToTA")) {
-                java.util.List<Member> move = message.getMentionedMembers();
-                for (Member member : move) {
-                    java.util.List<Role> curRole = member.getRoles();
-                    event.getGuild().addRoleToMember(member, ta).queue();
-                    event.getGuild().removeRoleFromMember(member, curRole.get(0)).queue();
-                    MessageChannel channel = event.getChannel();
-                    channel.sendMessage("Success!").queue();
-                }
-            }
-            if (content.startsWith("!changeToStudent")) {
-                java.util.List<Member> move = message.getMentionedMembers();
-                for (Member member : move) {
-                    java.util.List<Role> curRole = member.getRoles();
-                    event.getGuild().addRoleToMember(member, student).queue();
-                    event.getGuild().removeRoleFromMember(member, curRole.get(0)).queue();
-                    MessageChannel channel = event.getChannel();
-                    channel.sendMessage("Success!").queue();
-                }
-            }
+        if(Pattern.matches("!createVoiceChannel .*", content))
+        {
+            Member member = event.getMember();
+            createVoiceChannel(member, content.substring(20));
         }
-        else{
+        if(Pattern.matches("!moveMember .*", content))
+        {
+            Member member = event.getMember();
             MessageChannel channel = event.getChannel();
-            channel.sendMessage("You do not have permission to do this!").queue();
+            //VoiceChannel myVoiceChan = (VoiceChannel)event.getGuild().getVoiceChannelsByName(content.substring(11), false).toArray()[0];
+            //movePerson(member, (VoiceChannel)event.getGuild().getVoiceChannelsByName(content.substring(11), false).toArray()[0], event);
+            //MessageChannel channel = event.getChannel();
+            for(int i = 0; i < event.getGuild().getVoiceChannels().size(); i++)
+            {
+                String curResult = event.getGuild().getVoiceChannels().get(i).getName();
+                String inputo = content.substring(12);
+                if(curResult.equals(inputo)) {
+                    movePerson(member, event.getGuild().getVoiceChannels().get(i), event);
+                    return;
+                }
+                else
+                {
+                    //channel.sendMessage(event.getGuild().getVoiceChannels().get(i).getName()).queue();
+                }
+            }
+            channel.sendMessage("No voice channel with that name available!").queue();
+            //channel.sendMessage(Integer.toString(event.getGuild().getVoiceChannelsByName(content.substring(11), true).size())).queue();
+            //channel.sendMessage(Integer.toString(event.getGuild().getVoiceChannels().size())).queue();
         }
     }
     //Slash Commands
@@ -173,4 +146,87 @@ public class MyListener extends ListenerAdapter
         }
         return;
     }
+
+    public static String testOutput(String content)
+    {
+        if(Pattern.matches("!issueboard .+ (\\s|\\S)+", content))
+        {
+            int toSplit = 0;
+            int count = 0;
+            for(int i = 0; i < content.length(); i++)
+            {
+                if(content.charAt(i) == ' ')
+                {
+                    count += 1;
+                    if(count == 2)
+                    {
+                        toSplit = i;
+                        break;
+                    }
+                }
+            }
+            String fileName = content.substring(12, toSplit);
+            String issueText = content.substring(toSplit+1);
+            FileWriter myWriter = null;
+            try {
+                myWriter = new FileWriter("./issueBoards/" + (fileName + ".txt"), true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(myWriter != null)
+            {
+                try {
+                    myWriter.write(issueText + "\n");
+                    myWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            File textFile = null;
+            textFile = new File("./issueBoards/" + (fileName + ".txt"));
+            String wholeString = "";
+            try {
+                Scanner s = new Scanner(textFile).useDelimiter("\n");
+                int lineNumber = 1;
+                while(s.hasNextLine())
+                {
+                    String line = lineNumber + ". " + s.nextLine();
+                    wholeString += line;
+                    lineNumber += 1;
+                }
+                return wholeString;
+            } catch (FileNotFoundException e) {
+
+            }
+            return wholeString;
+        }
+        return "broken regex";
+    }
+    public static void createTextChannel(Member member, String name) {
+        Guild guild = member.getGuild();
+        guild.createTextChannel(name)
+                .addPermissionOverride(member, EnumSet.of(Permission.VIEW_CHANNEL), null)
+                .addPermissionOverride(guild.getPublicRole(), null, EnumSet.of(Permission.VIEW_CHANNEL))
+                .queue(); // this actually sends the request to discord.
+    }
+    public static void createVoiceChannel(Member member, String name) {
+        Guild guild = member.getGuild();
+        guild.createVoiceChannel(name)
+                .addPermissionOverride(member, EnumSet.of(Permission.VIEW_CHANNEL), null)
+                .addPermissionOverride(guild.getPublicRole(), null, EnumSet.of(Permission.VIEW_CHANNEL))
+                .queue(); // this actually sends the request to discord.
+    }
+    public static void movePerson(Member member, VoiceChannel voiceChannel, MessageReceivedEvent event)
+    {
+        MessageChannel channel = event.getChannel();
+        try {
+            event.getGuild().moveVoiceMember(member, voiceChannel).queue();
+        }
+        catch(Exception e)
+        {
+            channel.sendMessage("Missing Permissions").queue();
+        }
+    }
+
+
 }
